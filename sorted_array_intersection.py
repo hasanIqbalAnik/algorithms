2fff43bee58ba8246d9765914cd61bfa7bd5e5b7
Hasan Iqbal <hasan.iqbal.anik@gmail.com>
2015-09-17 00:51:27 -0500

    remove duplicates for sorted array intersection

--- sorted_array_intersection.py
+++ sorted_array_intersection.py
@@ -1,11 +1,11 @@
 '''
-time complexity of this algorithm: O(m+n) where m and n is the size of two sorted arrays.
+time complexity of this algorithm: O(m+n) where m and n is the size of two sorted arrays. it also removes duplicates
 
-idea from: http://articles.leetcode.com/2010/03/here-is-phone-screening-question-from.html
+idea from: http://art   icles.leetcode.com/2010/03/here-is-phone-screening-question-from.html
 '''
 
-a = [3, 4, 5, 7, 7, 9]
-b = [1, 2, 3, 7, 8, 8, 9]
+a = [1, 1, 2, 2, 3, 4, 5, 7, 7, 9, 10, 10]
+b = [1, 1, 2, 2, 3, 7, 8, 8, 9, 10, 10]
 intersect = []
 
 #a intersect b = [3, 7, 9]
@@ -15,10 +15,15 @@ lb = len(b)
 
 ia = 0
 ib = 0
+ic = 0
 
 while(ia < la and ib < lb):
     if(a[ia] == b[ib]): # a match!
-        intersect.append(a[ia])
+        if not intersect:
+            intersect.append(a[ia])
+        elif(intersect[ic] != a[ia]):
+            intersect.append(a[ia])
+            ic = ic + 1
         ia = ia + 1
         ib = ib + 1
     elif (a[ia] > b[ib]):
